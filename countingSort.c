5516547baf8c37816bd7d43bc0edb6daa0841b5b
mariaEdCaldas <127269057+mariaEdCaldas@users.noreply.github.com>
2023-09-03 21:01:22 -0400

    Update countingSort.c

--- countingSort.c
+++ countingSort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void countingSort(int array[], int tam){
-  int max = array[0]; 
+//implementando coutingSort
+void countingSort(int vetor[], int n){
+  int max = vetor[0]; 
    
-  for(int i = 1; i<tam; i++) {  
-    if(array[i] > max){
-      max = array[i];  
+  for(int i = 1; i<n; i++) {  
+    if(vetor[i] > max){
+      max = vetor[i];  
     }
   }
     
@@ -15,23 +16,20 @@ void countingSort(int array[], int tam){
   for (int i = 0; i <= max; ++i){  
     cont[i] = 0;
   }  
-    
-  for (int i = 0; i < tam; i++){  
-    cont[array[i]]++;  
+  for (int i = 0; i < n; i++){  
+    cont[vetor[i]]++;  
   }  
-  
   for(int i = 1; i<=max; i++) {  
     cont[i] += cont[i-1]; 
   }
 
-  int saida[tam];
+  int saida[n];
 
-  for (int i = tam - 1; i >= 0; i--) {  
-    saida[cont[array[i]] - 1] = array[i];  
-    cont[array[i]]--;
+  for (int i = n - 1; i >= 0; i--) {  
+    saida[cont[vetor[i]] - 1] = vetor[i];  
+    cont[vetor[i]]--;
   }
-  
-  for(int i = 0; i<tam; i++) {  
-    array[i] = saida[i]; 
+  for(int i = 0; i < n; i++) {  
+    vetor[i] = saida[i]; 
   }
 } 
